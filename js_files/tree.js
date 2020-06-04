832867e5f535e0134f2e6e62604f132abfcfaaed
Deepika <bhuvanadeepika017@gmail.com>
2020-06-04 14:48:48 +0530

    added deletion of node in js

--- js_files/tree.js
+++ js_files/tree.js
@@ -25,6 +25,43 @@ const search_node = (tree, value) => {
   return result;
 };
 
+const minOfRight = (root) => {
+  let p_walk = root;
+  while (p_walk.left != null) {
+    p_walk = p_walk.left;
+  }
+  return p_walk;
+};
+
+const deletion = (tree, value) => {
+  if (tree === null) {
+    return tree;
+  }
+  if (value < tree.value) {
+    tree.left = deletion(tree.left, value);
+    return tree;
+  }
+  if (value > tree.value) {
+    tree.right = deletion(tree.right, value);
+    return tree;
+  }
+  if (tree.left === null) {
+    let right_node = tree.right;
+    delete tree;
+    return right_node;
+  }
+  if (tree.right === null) {
+    let left_node = tree.left;
+    delete tree;
+    return left_node;
+  }
+  let min = minOfRight(tree.right);
+  tree.value = min.value;
+  tree.right = deletion(tree.right, min.value);
+  return tree;
+};
+
+
 const printInOrder = tree => {
   if (tree == null) {
     return;
